d9b12ea2949f9ea31e553af946bc56bc22ba9c48
이가영 <rk9547@naver.com>
2024-10-09 14:29:29 +0900

    clone_js_3

--- js_clone/app.js
+++ js_clone/app.js
@@ -4,14 +4,14 @@ const greeting = document.querySelector("#greeting")
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
-
+const USERNAME_KEY = "username"
 function LoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username)
     //greeting.innerText = "hello " + username;
-    greeting.innerText = `hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    paintGreetings(username)
     console.log(username);
 }
 
@@ -22,6 +22,20 @@ function handleLinkClick(event){
 }
 
 loginForm.addEventListener("submit",LoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
+
+function paintGreetings(){
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+    greeting.innerText = "hello " + savedUsername;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",LoginSubmit);
+}
+else{
+    paintGreetings(savedUsername)
+}
 //handleLinkClick({information about the event that just happened})
